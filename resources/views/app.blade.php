28f89ac6bd8fddc1866cdd5ae3e3413da2372ade
geek-3340 <crew.sento0864@gmail.com>
2026-07-07 13:34:22 +0900

    feat: 作成したロゴとアイコンを適用、github actionsを無効

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,8 +30,8 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/icon.png" sizes="any">
+        <link rel="icon" href="/icon.png" type="image/svg+xml"  >
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
@@ -40,9 +40,9 @@
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
             <title>{{ config('app.name', 'Laravel') }}</title>
-        </x-inertia::head>
+        </x-inertia::head>      
     </head>
     <body class="font-sans antialiased">
-        <x-inertia::app />
+        <x-inertia::app />  
     </body>
 </html>
